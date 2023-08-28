2518444bcf1422d1815290b819df33b5d5475fcf
fengwk <759543714@qq.com>
2023-08-28 22:01:00 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -11,6 +11,12 @@ sudo xbacklight -set 65
 # libinput-gestures-setup start
 libinput-gestures-setup restart
 
+# monitor
+dwm-switchmonitor 1
+
+# wallpaper
+dwm-defaultwallpaper
+
 # fcitx5
 killall -q fcitx5
 while pgrep -u $UID -x fcitx5 >/dev/null; do sleep 1; done
@@ -26,11 +32,6 @@ killall -q dwm-status.sh
 while pgrep -u $UID -x dwm-status.sh >/dev/null; do sleep 1; done
 ${script_dir}/dwm-status.sh &
 
-# wallpaper
-killall -q wp-autochange.sh
-while pgrep -u $UID -x wp-autochange.sh >/dev/null; do sleep 1; done
-${script_dir}/wp-autochange.sh &
-
 # window render
 killall -q picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
@@ -38,11 +39,6 @@ while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 # picom -b --experimental-backends --transparent-clipping
 # picom -b
 picom --experimental-backends -b
-
-# bluez
-# killall -q blueman-applet
-# while pgrep -u $UID -x blueman-applet >/dev/null; do sleep 1; done
-# blueman-applet &
 
 # network
 killall -q nm-applet
@@ -59,19 +55,6 @@ killall -q xfce4-power-manager
 while pgrep -u $UID -x xfce4-power-manager >/dev/null; do sleep 1; done
 xfce4-power-manager &
 
-# volum
-# killall -q xfce4-volumed-pulse
-# while pgrep -u $UID -x xfce4-volumed-pulse >/dev/null; do sleep 1; done
-# xfce4-volumed-pulse &
-
-# parcellite
-# killall -q parcellite
-# while pgrep -u $UID -x parcellite >/dev/null; do sleep 1; done
-# parcellite &
-
-# gpaste
-# gpaste-client start
-
 # clipster
 killall -q clipster
 while pgrep -u $UID -x clipster >/dev/null; do sleep 1; done
@@ -80,18 +63,13 @@ clipster -d &
 # clash for windows
 # killall -q /opt/clash-for-windows-chinese/cfw
 # while pgrep -u $UID -x /opt/clash-for-windows-chinese/cfw >/dev/null; do sleep 1; done
-# sleep 2.5 # 迟启动调整托盘图标出现位置
+# sleep 3 # 迟启动调整托盘图标出现位置
 # /opt/clash-for-windows-chinese/cfw &
 
 # optimus
 # killall optimus-manager-qt
 # while pgrep -u $UID -x optimus-manager-qt >/dev/null; do sleep 1; done
 # optimus-manager-qt &
-
-# slimbookbattery
-# killall slimbookbattery
-# while pgrep -u $UID -x slimbookbattery >/dev/null; do sleep 1; done
-# slimbookbattery &
 
 # wxwork
 # killall -q wxwork
@@ -103,8 +81,3 @@ clipster -d &
 # while pgrep -u $UID -x birdtray >/dev/null; do sleep 1; done
 # # env -u QT_AUTO_SCREEN_SCALE_FACTOR birdtray &
 # birdtray &
-
-# lightscreen
-# killall -q lightscreen
-# while pgrep -u $UID -x lightscreen >/dev/null; do sleep 1; done
-# lightscreen &
--- autostop_blocking.sh
+++ autostop_blocking.sh
@@ -11,14 +11,8 @@ killall -q /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 # status bar
 killall -q dwm-status.sh
 
-# wallpaper
-killall -q wp-autochange.sh
-
 # window render
 killall -q picom
-
-# bluez
-# killall -q blueman-applet
 
 # network
 killall -q nm-applet
@@ -29,38 +23,20 @@ killall -q nm-applet
 # power
 killall -q xfce4-power-manager
 
-# volum
-# killall -q xfce4-volumed-pulse
-
-# parcellite
-# killall -q parcellite
-
-# gpaste
-# killall -q gpaste-daemon
-
 # clipster
 killall -q clipster
 
 # clash-for-windows-chinese
 # killall -q /opt/clash-for-windows-chinese/cfw
 
-# parcellite
-# killall -q parcellite
-
 # optimus
 # killall optimus-manager-qt
-
-# slimbookbattery
-# killall slimbookbattery
 
 # wxwork
 # killall -q wxwork
 
 # birdtray
 # killall -q birdtray
-
-# lightscreen
-# killall -q lightscreen
 
 # optimus-manager
 # https://github.com/Askannz/optimus-manager/issues/404
